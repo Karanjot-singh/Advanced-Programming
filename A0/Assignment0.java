8e31227ffe91de4c8593ec2bc471f45d71f3d9a8
karanjot-singh <karanjot19050@iiitd.ac.in>
2020-08-20 02:47:52 +0530

    updated Assignment with basic GUI

--- A0/Assignment0.java
+++ A0/Assignment0.java
@@ -2,6 +2,8 @@ package A0;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Date;
 class Patient {
 
@@ -108,55 +110,62 @@ public class Assignment0 {
 }
 
     class SwingApp extends JFrame{
-        JLabel label, label2;
+        JLabel label, label2,label3;
         JTextField text1, text2, text3;
-        JButton button1, button2;
-        JRadioButton rbtn1, rbtn2;
-        JCheckBox cbox1;
+        JButton button1;
+        JCheckBox boxA , boxB, boxC, boxD;
 
         SwingApp() {
-            label = new JLabel("First number ");
+            label = new JLabel("                         Assignment 0                      " );
             label2 = new JLabel();
-            label2.setText("Second number");
+            label2.setText("Enter the Date");
+            label3 = new JLabel("Check the Towers");
+
 
             text1 = new JTextField(20);
             text2 = new JTextField(20);
             text3 = new JTextField(20);
 
-            button1 = new JButton("ADD");
-            button2 = new JButton("MUL");
+            //BUTTON
+            button1 = new JButton("Display");
 
     //        button1.addActionListener(this);
-    //        button2.addActionListener(this);
 
-            rbtn1 = new JRadioButton("Yes");
-            rbtn2 = new JRadioButton("NO");
-
-            //to add single select constraint
-            ButtonGroup buttonGroup = new ButtonGroup();
-            buttonGroup.add(rbtn1);
-            buttonGroup.add(rbtn2);
-
-    //        rbtn1.addActionListener(this);
-    //        rbtn2.addActionListener(this);
-
-    //        cbox1 = new JCheckBox();
-            add(rbtn1);
-            add(rbtn2);
+            //start
+            button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+//                int a = Integer.parseInt(text1.getText());
+//                int b = Integer.parseInt(text2.getText());
+//                text3.setText((a+b) +"");
 
 
-            setTitle("demo");
+            }
+        });
+            //end
 
-            // change layout for stack
-            //flow grid
-    //        Default layout is card layout
+            //checkbox
+            boxA = new JCheckBox("A");
+            boxB = new JCheckBox("B");
+            boxC = new JCheckBox("C");
+            boxD = new JCheckBox("D");
+
+            //Add
+//            add(label);
+            add(label2);
+            add(text2);
+            add(label3);
+            add(boxA);
+            add(boxB);
+            add(boxC);
+            add(boxD);
+            add(button1);
+            setTitle("Karanjot Singh");
             FlowLayout f = new FlowLayout();
             setLayout(f);
-
-            // IMPT for
+            // IMPT
             setSize(380, 400);
             setVisible(true);
-            //to stop the application
             setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         }
