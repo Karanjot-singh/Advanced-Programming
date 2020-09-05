0f9f8772f5e3c11fce239fe641e4943715ab46f1
karanjot-singh <karanjot19050@iiitd.ac.in>
2020-09-05 13:35:01 +0530

    Assignment 1

--- Assignment1/Assignment.java
+++ Assignment1/Assignment.java
@@ -15,8 +15,43 @@ public class Assignment {
     		int inputOxygen = sc.nextInt();
     		int inputAge = sc.nextInt();
     		Patient p = new Patient(inputName,inputAge,inputOxygen,inputTemp);
-    		record.add(p);
-    		
+    		record.add(p);    		
+    	}
+    	int loopFlag=1;
+    	while(loopFlag) {
+    		int inputQuery = sc.nextInt();
+    		switch (inputQuery) {
+    		  case 0:
+    			  loopFlag=0;
+    			  break;    			
+    		  case 1:
+    		    System.out.println("Monday");
+    		    break;
+    		  case 2:
+    		    System.out.println("Tuesday");
+    		    break;
+    		  case 3:
+    		    System.out.println("Wednesday");
+    		    break;
+    		  case 4:
+    		    System.out.println("Thursday");
+    		    break;
+    		  case 5:
+    		    System.out.println("Friday");
+    		    break;
+    		  case 6:
+    		    System.out.println("Saturday");
+    		    break;
+    		  case 7:
+    		    System.out.println("Sunday");
+    		    break;
+    		  case 8:
+      		    System.out.println("Sunday");
+      		    break;
+    		  case 9:
+      		    System.out.println("Sunday");
+      		    break;
+    		}
     	}
         
     }
