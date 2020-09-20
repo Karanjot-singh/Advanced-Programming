397eb865ddd3618d59a9f6f17c1c1b8d85f854ad
karanjot-singh <karanjot19050@iiitd.ac.in>
2020-09-20 17:47:26 +0530

    Basic class structure defined

--- assignment2/Assignment2.java
+++ assignment2/Assignment2.java
@@ -25,25 +25,39 @@ class App{
 	App(){
 		//hardcode data
 	}
+	
+	public static ArrayList<Customer> getCustomerRecord() {
+		return customerRecord;
+	}
+
+	public static ArrayList<Restaurant> getRestaurantRecord() {
+		return restaurantRecord;
+	}
+
 }
 
 abstract class User{
 
-	protected String name;
-	protected String address;
+	protected final String name;
+	protected final String address;	
 	public User(String name, String address) {
 		super();
 		this.name = name;
 		this.address = address;
 	}
-	
+	public String getName() {
+		return name;
+	}
+	public String getAddress() {
+		return address;
+	}
 	
 }
 
 class Restaurant extends User{
 	protected static ArrayList <Food> menu = new ArrayList<Food>();
-	protected double discount;
 	protected int numberOrders=0;
+	protected double discount;
 	protected double morediscountCriteria,morediscountAmount;
 	protected double rewardAmount, rewardCriteria;
 	public Restaurant(String name, String address) {
@@ -54,6 +68,28 @@ class Restaurant extends User{
 		this.rewardAmount=5;
 		this.rewardCriteria=100;
 	}
+	public static ArrayList<Food> getMenu() {
+		return menu;
+	}
+	public double getDiscount() {
+		return discount;
+	}
+	public int getNumberOrders() {
+		return numberOrders;
+	}
+	public double getMorediscountCriteria() {
+		return morediscountCriteria;
+	}
+	public double getMorediscountAmount() {
+		return morediscountAmount;
+	}
+	public double getRewardAmount() {
+		return rewardAmount;
+	}
+	public double getRewardCriteria() {
+		return rewardCriteria;
+	}
+	
 	
 }
 class AuthenticRestaurant extends Restaurant{
@@ -81,6 +117,7 @@ class FastfoodRestaurant extends Restaurant{
 }
 
 class Customer extends User{
+	private String category;
 	protected static ArrayList <Restaurant> restaurants = new ArrayList<Restaurant>();
 	protected ArrayList <Cart> pastOrders = new ArrayList<Cart>();
 	protected Restaurant selectedRestaurant;
@@ -89,16 +126,73 @@ class Customer extends User{
 	protected double discountCriteria,discountAmount;
 	protected double rewardAmount,wallet;
 	
-	
+	public Customer(String name, String address, String category) {
+		super(name, address);
+		this.category = category;
+		this.wallet = 1000;
+		this.discountAmount=0;
+		this.discountCriteria=0;		
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public static ArrayList<Restaurant> getRestaurants() {
+		return restaurants;
+	}
+
+	public ArrayList<Cart> getPastOrders() {
+		return pastOrders;
+	}
+
+	public Restaurant getSelectedRestaurant() {
+		return selectedRestaurant;
+	}
+
+	public Cart getCurrentOrder() {
+		return currentOrder;
+	}
+
+	public int getDelivery() {
+		return delivery;
+	}
+
+	public double getDiscountCriteria() {
+		return discountCriteria;
+	}
+
+	public double getDiscountAmount() {
+		return discountAmount;
+	}
+
+	public double getRewardAmount() {
+		return rewardAmount;
+	}
+
+	public double getWallet() {
+		return wallet;
+	}
 	
 	
 }
 class EliteCustomer extends Customer{
-	
-	
-	
+
+	public EliteCustomer(String name, String address, String category) {
+		super(name, address, category);
+		this.discountAmount=50;
+		this.discountCriteria=200;
+		this.delivery=0;
+	}	
 }
-class SpecialCustomer extends Customer{	
+class SpecialCustomer extends Customer{
+
+	public SpecialCustomer(String name, String address, String category) {
+		super(name, address, category);
+		this.discountAmount=25;
+		this.discountCriteria=200;
+		this.delivery=20;
+	}	
 	
 	
 }
