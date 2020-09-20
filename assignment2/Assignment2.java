87a6f98d4ea1d028e19724bdd600fce7e068c772
karanjot-singh <karanjot19050@iiitd.ac.in>
2020-09-20 15:13:46 +0530

    Added Assignment 2 Files

---
+++ assignment2/Assignment2.java
@@ -0,0 +1,27 @@
+package assignment2;
+import java.util.ArrayList;
+
+public class Assignment2 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
+
+class Zotato{
+	double deliveryCharges;
+	double restaurantCharges;
+	
+}
+class App{
+	protected static ArrayList <Customer> customerRecord = new ArrayList<Customer>();
+	protected static ArrayList <Restaurant> restaurantRecord = new ArrayList<Restaurant>();
+}
+
+class Restaurant{
+	
+}
+class Customer{
+	
+}
