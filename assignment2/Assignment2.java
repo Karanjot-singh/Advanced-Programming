09b5b5c57fbb54f0da79b498a2153d03c920238e
karanjot-singh <karanjot19050@iiitd.ac.in>
2020-09-20 17:23:02 +0530

    Added new classes

--- assignment2/Assignment2.java
+++ assignment2/Assignment2.java
@@ -45,11 +45,14 @@ class Restaurant extends User{
 	protected double discount;
 	protected int numberOrders=0;
 	protected double morediscountCriteria,morediscountAmount;
+	protected double rewardAmount, rewardCriteria;
 	public Restaurant(String name, String address) {
 		super(name, address);
 		this.discount = 0;
 		this.morediscountCriteria = 0;
 		this.morediscountAmount = 0;
+		this.rewardAmount=5;
+		this.rewardCriteria=100;
 	}
 	
 }
@@ -60,6 +63,8 @@ class AuthenticRestaurant extends Restaurant{
 		this.discount =discount ;
 		this.morediscountCriteria = 100;
 		this.morediscountAmount = 50;
+		this.rewardAmount=25;
+		this.rewardCriteria=200;
 	}	
 }
 class FastfoodRestaurant extends Restaurant{
@@ -69,11 +74,44 @@ class FastfoodRestaurant extends Restaurant{
 		this.discount =discount ;
 		this.morediscountCriteria = 0;
 		this.morediscountAmount = 0;
+		this.rewardAmount=10;
+		this.rewardCriteria=150;
 	}
 	
 }
 
 class Customer extends User{
+	protected static ArrayList <Restaurant> restaurants = new ArrayList<Restaurant>();
+	protected ArrayList <Cart> pastOrders = new ArrayList<Cart>();
+	protected Restaurant selectedRestaurant;
+	protected Cart currentOrder;		
+	protected int delivery=40;
+	protected double discountCriteria,discountAmount;
+	protected double rewardAmount,wallet;
+	
+	
+	
+	
+}
+class EliteCustomer extends Customer{
+	
+	
+	
+}
+class SpecialCustomer extends Customer{	
+	
+	
+}
+
+class Cart{
+	private double totalAmount;
+	private ArrayList <Food> items = new ArrayList<Food>();
+	public Cart(ArrayList<Food> items) {
+		super();
+		this.items = items;
+		//code to calculate discounts, checkout , rewards
+	}
+	
 	
 	
 }
@@ -81,20 +119,34 @@ class Customer extends User{
 class Food{
 	private static int idGenerator=1;
 	private int id;
-	private String name;
+	private String name, category;
 	private double price, discount;
 	private int quantity;
 
-	public Food(String name, double price, double discount, int quantity) {
+	public Food(String name, String category, double price, double discount, int quantity) {
 		super();
 		this.name = name;
 		this.price = price;
 		this.discount = discount;
 		this.quantity = quantity;
+		this.category=category;
 		this.id=Food.idGenerator;
 		Food.idGenerator++;
 		
 	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
 	public String getName() {
 		return name;
 	}
