2bf49e4a6f21d39c997ceb14d000d96776616dd4
karanjot-singh <karanjot19050@iiitd.ac.in>
2020-10-03 19:45:22 +0530

    Initialised files for Assignment3

---
+++ assignment3/Assignment3.java
@@ -0,0 +1,23 @@
+package assignment3;
+
+import java.util.ArrayList;
+
+public class Assignment3 {
+}
+
+class Game {
+    private static ArrayList<Player> players;
+
+    public Game(ArrayList<Player> players) {
+
+    }
+}
+
+abstract class Player {
+    private static ArrayList<Player> players;
+
+    public static int generateRandom() {
+        return 0;
+    }
+}
+
