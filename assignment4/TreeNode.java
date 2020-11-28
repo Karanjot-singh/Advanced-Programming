6d155ceb132e7820d2bcffe3916651ed54f3885d
karanjot-singh <karanjot19050@iiitd.ac.in>
2020-11-29 03:02:30 +0530

    Assignment 4 step1 code added

---
+++ assignment4/TreeNode.java
@@ -0,0 +1,35 @@
+package assignment4;
+
+import java.util.ArrayList;
+
+public class TreeNode {
+	private Integer data;
+	private ArrayList<TreeNode> children;
+
+	public TreeNode() {
+		this.data = (int) (Math.random() * 1000000 + 1);
+		System.out.println(this.data);
+		this.children = new ArrayList<>();
+	}
+
+	public Integer getData() {
+		return data;
+	}
+
+	public void setData(Integer data) {
+		this.data = data;
+	}
+
+	public ArrayList<TreeNode> getChildren() {
+		return children;
+	}
+
+	public void setChildren(ArrayList<TreeNode> children) {
+		this.children = children;
+	}
+
+	public void traverseForkJoinPool(){
+
+	}
+
+}
